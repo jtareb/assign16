14a7251e4c3a6c28a9da2f3e941f08cce6585ab4
John Albritton <jtareb1@att.net>
2015-02-10 07:24:49 -0500

    troubleshooting

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,4 +1,10 @@
-var data = {
-	city: 'Atlanta'
-	state: 'Georgia'
-};
+ var data = { nameinfo: [
+      {username: "john", firstName: "John", lastName: "Albritton", email: "jtareb1@gmail.com" },
+      
+    ]};
+
+console.log(data);
+
+ var source   = $("#nameinfo").html();
+ var template = Handlebars.compile(source);
+  $("#nameinfo").html(template(data));
