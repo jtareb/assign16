95adf9352527117dd5ec0fff6a8517a6de5feebc
John Albritton <jtareb1@att.net>
2015-02-10 08:04:19 -0500

    final

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,10 +1,14 @@
  var data = { nameinfo: [
       {username: "john", firstName: "John", lastName: "Albritton", email: "jtareb1@gmail.com" },
-      
+      {username: "ed", firstName: "Ed", lastName: "Smith", email: "edsmith2@gmail.com"},
     ]};
 
 console.log(data);
 
  var source   = $("#nameinfo").html();
+ console.log(source);
  var template = Handlebars.compile(source);
   $("#nameinfo").html(template(data));
+
+
+//*var template = Handlebars.tempate(source);
